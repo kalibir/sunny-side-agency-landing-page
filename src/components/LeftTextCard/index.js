a2c1ed8446e3860d376a62929c1df59fc9f1620e
antonio.meireles <edgarmeireles68@gmail.com>
2021-11-16 15:49:55 +0100

    left text card mobile

---
+++ src/components/LeftTextCard/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./styles.scss";
+
+const LeftTextCard = () => {
+	return (
+		<div className='info-wrapper'>
+			<div className='image-holder'></div>
+			<div className='text-holder'>
+				<h3>Transform your brand</h3>
+				<p>
+					We are a full-service creative agency specializing in helping brands
+					grow fast. Engage your clients through compelling visuals that do most
+					of the marketing for you.
+				</p>
+				<a href='#'>Learn more</a>
+			</div>
+		</div>
+	);
+};
+
+export default LeftTextCard;
