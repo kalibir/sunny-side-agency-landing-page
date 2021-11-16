733b57b26879a183e0623f89b64334f19e680139
antonio.meireles <edgarmeireles68@gmail.com>
2021-11-16 13:11:02 +0100

    mobile header and renamed folders

---
+++ src/components/Header/index.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./styles.scss";
+import logo from "../../assets/images/logo.svg";
+import arrow from "../../assets/images/icon-arrow-down.svg";
+
+const Header = () => {
+	const [open, isOpen] = useState(false);
+
+	const handleClick = () => {
+		isOpen(!open);
+	};
+
+	return (
+		<div className='header'>
+			<div className='nav'>
+				<a href='#'>
+					<img src={logo} alt='logo' />
+				</a>
+				<div className='hamburguer-menu' onClick={handleClick}>
+					<span className='bar'></span>
+					<span className='bar'></span>
+					<span className='bar'></span>
+				</div>
+			</div>
+			{open ? (
+				<div className='mobile-menu'>
+					<ul>
+						<li>
+							<a href='#'>About</a>
+						</li>
+						<li>
+							<a href='#'>Services</a>
+						</li>
+						<li>
+							<a href='#'>Projects</a>
+						</li>
+						<li>
+							<a href='#'>Contact</a>
+						</li>
+					</ul>
+				</div>
+			) : null}
+			<div className='header-text'>
+				<h2>We are creatives</h2>
+				<img src={arrow} />
+			</div>
+		</div>
+	);
+};
+
+export default Header;
